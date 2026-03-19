744ecf1bc640078f330c76787bc702fbd47f1fbc
Janakiraman-311 <Janakiraman.bt@gmail.com>
2026-03-19 20:51:56 +0530

    Phase 26: Add diagnostic logging and tool verification for CI launcher build

--- launcher_src/launcher/build_launcher.bat
+++ launcher_src/launcher/build_launcher.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 set "RTOOLS_BIN=C:\rtools45\x86_64-w64-mingw32.static.posix\bin"
 set "RTOOLS_USR=C:\rtools45\usr\bin"
 if not exist "%RTOOLS_BIN%" (
@@ -7,17 +7,33 @@ if not exist "%RTOOLS_BIN%" (
 )
 set "PATH=%RTOOLS_BIN%;%RTOOLS_USR%;%PATH%"
 
+echo --- DIAGNOSTICS ---
+echo PATH: %PATH%
+cmake --version
+g++ --version
+gcc --version
+
 set "CMAKE=cmake"
 set "GXX=%RTOOLS_BIN%\g++.exe"
 set "GCC=%RTOOLS_BIN%\gcc.exe"
-set "MAKE=%RTOOLS_USR%\make.exe"
+set "MAKE=%RTOOLS_BIN%\mingw32-make.exe"
+
+if not exist "%MAKE%" set "MAKE=%RTOOLS_USR%\make.exe"
+
+echo USING CMAKE: %CMAKE%
+echo USING GXX: %GXX%
+echo USING GCC: %GCC%
+echo USING MAKE: %MAKE%
 
 REM Go to script directory
 cd /d "%~dp0"
+echo CURRENT DIR: %CD%
+
 if exist build rmdir /s /q build
 mkdir build
 cd build
 
+echo --- CONFIGURING ---
 "%CMAKE%" .. -G "MinGW Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_CXX_COMPILER="%GXX%" ^
@@ -26,13 +42,15 @@ cd build
     -DCMAKE_SH="CMAKE_SH-NOTFOUND"
 
 if errorlevel 1 (
-    echo CMake failed
+    echo CMake failed. Listing directory for debugging:
+    dir .. /s
     exit /b 1
 )
 
-"%CMAKE%" --build . --config Release
+echo --- BUILDING ---
+"%CMAKE%" --build . --config Release --verbose
 if errorlevel 1 (
-    echo Build failed
+    echo Build failed.
     exit /b 1
 )
 
